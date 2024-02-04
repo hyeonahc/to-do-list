6badd2578b617ca3491d68b4ee5c017c20bb8636
Hyeonah Cho <hyeonah@Hyeonahs-Air.ht.home>
2024-02-03 23:00:52 -0500

    Introduce state for menu creation

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,27 @@
 import { MoonIcon, SunIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react'
+
+interface Menu {
+  text: string
+  clicked: boolean
+}
 
 const TopBar = () => {
+  const [taskStatusMenus, setTaskStatusMenus] = useState<Menu[]>([
+    {
+      text: 'All',
+      clicked: true,
+    },
+    {
+      text: 'Active',
+      clicked: false,
+    },
+    {
+      text: 'Completed',
+      clicked: false,
+    },
+  ])
+
   return (
     <div className='bg-lightgrey flex justify-between p-3 rounded-t-lg'>
       <div>
@@ -8,9 +29,16 @@ const TopBar = () => {
         <MoonIcon className='h-6 w-6 text-zinc-500 hidden' />
       </div>
       <div className='flex gap-x-3 text-primaryWithOpacity'>
-        <div className='text-primary border-b-2 border-primary'>All</div>
-        <div>Active</div>
-        <div>Completed</div>
+        {taskStatusMenus.map((menu, index) => (
+          <div
+            key={index}
+            className={
+              menu.clicked ? 'text-primary border-b-2 border-primary' : ''
+            }
+          >
+            {menu.text}
+          </div>
+        ))}
       </div>
     </div>
   )
